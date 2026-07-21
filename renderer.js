f2fc7ba834c64668d1a61e6d7317ee130048621a
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-07-22 00:43:07 +0100

    Begin renderer structure

---
+++ renderer.js
@@ -0,0 +1,16 @@
+class Renderer {
+    constructor(ctx, board) {
+        this.ctx = ctx;
+        this.board = board;
+    }
+
+    drawBoard() {
+        const rows = this.board.rows;
+        const ctx = this.ctx;
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < rows; j++) {
+                
+            }
+        }
+    }
+}
