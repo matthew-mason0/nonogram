eb1f197bda09723f749522a98fb07da9e6f49604
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-07-23 15:46:53 +0100

    Add async main loop to load puzzle

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 class Game {
-    constructor(ctx) {
+    constructor(ctx, puzzle) {
         this.board = new Board(5);
         this.renderer = new Renderer(ctx, this.board)
-
+        this.puzzle = puzzle;
         console.log(this.puzzle);
 
         this.renderer.drawBoard();
@@ -10,7 +10,7 @@ class Game {
 
     mousePressed(x, y) {
         console.log(`mouse press at: ${x}, ${y}`);
-        const [rowPressed, colPressed] = this.renderer.getCellCoordsAt(x, y);
+        const {rowPressed, colPressed} = this.renderer.getCellCoordsAt(x, y);
         this.board.toggleCell(rowPressed, colPressed);
         this.renderer.drawBoard();
     }
