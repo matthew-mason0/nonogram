05041f64fdf609a6360ee62a9ca1c813a2b5a2eb
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-07-23 16:00:48 +0100

    Update json format

--- puzzle.js
+++ puzzle.js
@@ -1,20 +1,19 @@
 class Puzzle {
     constructor(data) {
         this.name = data.name;
-        this.width = data.width;
-        this.height = data.height;
+        this.rows = data.rows;
         this.rowClues = data.rowClues;
         this.columnClues = data.columnClues;
         this.solution = data.solution;
     }
 
     validate() {
-        if (this.solution.length !== this.height) {
+        if (this.solution.length !== this.rows) {
             console.log("Puzzle: Solution height does not match puzzle height");
         }
 
         for (const row of this.solution) {
-            if (row.length !== this.width) {
+            if (row.length !== this.rows) {
                 console.log("Puzzle: Solution width does not match puzzle width");
             }
         }
