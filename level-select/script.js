5ae8f38abbebc0f99755f3288fa35b62daff718a
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-08-18 00:22:49 +0100

    Add back button to level selector page

--- level-select/script.js
+++ level-select/script.js
@@ -1,3 +1,8 @@
+const backButton = document.getElementById('back-button');
+backButton.addEventListener("click", () => {
+    window.location.href = `../index.html`;
+});
+
 const levelGrid = document.getElementById("level-grid");
 const buttons = levelGrid.getElementsByTagName("button");
 
