99493671ab464d72492443d913da290137bf40b5
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-08-19 23:29:28 +0100

    Get HUD elements

---
+++ play/hud.js
@@ -0,0 +1,6 @@
+const levelDisplay = document.getElementById("level-display");
+const timer = document.getElementById("timer");
+const pauseButton = document.getElementById("pause-button");
+const restartButton = document.getElementById("restart-button");
+const exitButton = document.getElementById("exit-button");
+
