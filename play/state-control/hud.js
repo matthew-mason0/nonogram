6f15218b97769fc877b35049581ab63bdeffa5ec
Matthew Mason <matthewmason2004@yahoo.co.uk>
2026-08-21 09:37:51 +0100

    Pause toggle implemented

--- play/state-control/hud.js
+++ play/state-control/hud.js
@@ -7,9 +7,7 @@ const restartButton = document.getElementById("restart-button");
 const exitButton = document.getElementById("exit-button");
 
 pauseButton.addEventListener("click", (event) => {
-    console.log("pause button pressed");
-    // test
-    console.log(gameState);
+    gameState.togglePause();
 });
 restartButton.addEventListener("click", (event) => {
     console.log("restart button pressed");
